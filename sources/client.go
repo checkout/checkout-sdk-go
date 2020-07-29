cf414c12d61c53df112a132474a97b585b73cf24
shiuh-yaw-cko <shiuhyaw.phang@checkout.com>
2020-07-29 14:55:20 +0800

    Title: Updating Source for ACH & SEPA
    Body:
    
    Co-authored-by: ShiuhYaw <shiuhyaw.phang@checkout.com>

---
+++ sources/client.go
@@ -0,0 +1,40 @@
+package sources
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/shiuh-yaw-cko/checkout"
+	"github.com/shiuh-yaw-cko/checkout/httpclient"
+)
+
+const path = "sources"
+
+// Client ...
+type Client struct {
+	API checkout.HTTPClient
+}
+
+// NewClient ...
+func NewClient(config checkout.Config) *Client {
+	return &Client{
+		API: httpclient.NewClient(config),
+	}
+}
+
+// AddPaymentSource -
+func (c *Client) AddPaymentSource(request *Request) (*Response, error) {
+	response, err := c.API.Post("/"+path, request)
+	resp := &Response{
+		StatusResponse: response,
+	}
+	if err != nil {
+		return resp, err
+	}
+	if response.StatusCode == http.StatusCreated {
+		var source Source
+		err = json.Unmarshal(response.ResponseBody, &source)
+		resp.Source = &source
+	}
+	return resp, err
+}
---
+++ sources/source.go
@@ -0,0 +1,83 @@
+package sources
+
+import (
+	"github.com/shiuh-yaw-cko/checkout"
+	"github.com/shiuh-yaw-cko/checkout/common"
+)
+
+type (
+	// Request -
+	Request struct {
+		*SEPA
+		*ACH
+	}
+
+	// SEPA -
+	SEPA struct {
+		Type           string          `json:"type" binding:"required"`
+		Reference      string          `json:"reference,omitempty"`
+		Phone          *common.Phone   `json:"phone,omitempty"`
+		Customer       *Customer       `json:"customer,omitempty"`
+		BillingAddress *common.Address `json:"billing_address" binding:"required"`
+		SourceData     *SEPASourceData `json:"source_data,omitempty"`
+	}
+
+	// ACH -
+	ACH struct {
+		Type           string          `json:"type" binding:"required"`
+		Reference      string          `json:"reference,omitempty"`
+		Phone          *common.Phone   `json:"phone,omitempty"`
+		Customer       *Customer       `json:"customer,omitempty"`
+		BillingAddress *common.Address `json:"billing_address" binding:"required"`
+		SourceData     *ACHSourceData  `json:"source_data,omitempty"`
+	}
+
+	// Customer -
+	Customer struct {
+		ID    string `json:"id,omitempty"`
+		Email string `json:"email,omitempty"`
+		Name  string `json:"name,omitempty"`
+	}
+
+	// SEPASourceData -
+	SEPASourceData struct {
+		FirstName         string `json:"first_name,omitempty"`
+		LastName          string `json:"last_name,omitempty"`
+		AccountIBAN       string `json:"account_iban,omitempty"`
+		BIC               string `json:"bic,omitempty"`
+		BillingDescriptor string `json:"billing_descriptor,omitempty"`
+		MandateType       string `json:"mandate_type,omitempty"`
+	}
+
+	// ACHSourceData -
+	ACHSourceData struct {
+		AccountType       string `json:"account_type,omitempty"`
+		AccountNumber     string `json:"account_number,omitempty"`
+		RoutingNumber     string `json:"routing_number,omitempty"`
+		AccountHolderName string `json:"account_holder_name,omitempty"`
+		BillingDescriptor string `json:"billing_descriptor,omitempty"`
+		CompanyName       string `json:"company_name,omitempty"`
+	}
+)
+
+type (
+	// Response -
+	Response struct {
+		StatusResponse *checkout.StatusResponse `json:"api_response,omitempty"`
+		Source         *Source                  `json:"source,omitempty"`
+	}
+	// Source -
+	Source struct {
+		ID           string                 `json:"id,omitempty"`
+		Type         string                 `json:"type,omitempty"`
+		ResponseCode string                 `json:"response_code,omitempty"`
+		Customer     *Customer              `json:"response_data,omitempty"`
+		ResponseData *ResponseData          `json:"uploaded_on,omitempty"`
+		Links        map[string]common.Link `json:"_links"`
+	}
+
+	// ResponseData -
+	ResponseData struct {
+		MandateReference string `json:"mandate_reference,omitempty"`
+	}
+)

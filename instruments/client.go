adf7a2580c392d135cec2be78b40e46f4c7a553d
shiuh-yaw-cko <shiuhyaw.phang@checkout.com>
2020-09-15 22:26:05 +0800

    Title: Introducing instruments API
    Body:
    
    Co-authored-by: ShiuhYaw <shiuhyaw.phang@checkout.com>
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   instruments/client.go
    	new file:   instruments/instrument.go

---
+++ instruments/client.go
@@ -0,0 +1,80 @@
+package instruments
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/shiuh-yaw-cko/checkout"
+	"github.com/shiuh-yaw-cko/checkout/httpclient"
+	"github.com/shiuh-yaw-cko/checkout/payments"
+)
+
+const path = "instruments"
+
+// Client ...
+type Client struct {
+	API checkout.HTTPClient
+}
+
+// NewClient ...
+func NewClient(config checkout.Config) *Client {
+	return &Client{
+		API: httpclient.NewClient(config),
+	}
+}
+
+// Create an instrument
+func (c *Client) Create(request *Request) (*Response, error) {
+	resp, err := c.API.Post("/"+path, request, nil)
+	response := &Response{
+		StatusResponse: resp,
+	}
+	if err != nil {
+		return response, err
+	}
+	if resp.StatusCode == http.StatusCreated {
+		var source payments.SourceResponse
+		err = json.Unmarshal(resp.ResponseBody, &source)
+		response.Source = &source
+		return response, err
+	}
+	return response, err
+}
+
+// Get instrument details
+func (c *Client) Get(sourceID string) (*Response, error) {
+
+	resp, err := c.API.Get(fmt.Sprintf("/%v/%v", path, sourceID))
+	response := &Response{
+		StatusResponse: resp,
+	}
+	if err != nil {
+		return response, err
+	}
+	if resp.StatusCode == http.StatusOK {
+		var source payments.SourceResponse
+		err = json.Unmarshal(resp.ResponseBody, &source)
+		response.Source = &source
+		return response, err
+	}
+	return response, err
+}
+
+// Update instrument details
+func (c *Client) Update(sourceID string, request *Request) (*Response, error) {
+	resp, err := c.API.Patch(fmt.Sprintf("/%v/%v", path, sourceID), request)
+	response := &Response{
+		StatusResponse: resp,
+	}
+	if err != nil {
+		return response, err
+	}
+	if resp.StatusCode == http.StatusOK {
+		var instrumentResponse InstrumentResponse
+		err = json.Unmarshal(resp.ResponseBody, &instrumentResponse)
+		response.InstrumentResponse = &instrumentResponse
+		return response, err
+	}
+	return response, err
+}

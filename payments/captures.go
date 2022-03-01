65b5e1a01caf096ffcaa6713d6a877dce218d81c
Raul Estrela <rsaestrela@gmail.com>
2022-03-01 10:49:21 +0100

    Add support for Captures `capture_type`

--- payments/captures.go
+++ payments/captures.go
@@ -1,12 +1,29 @@
 package payments
 
-import "github.com/checkout/checkout-sdk-go"
+import (
+	"github.com/checkout/checkout-sdk-go"
+)
+
+// CaptureType ...
+type CaptureType string
+
+const (
+	// NonFinal ...
+	NonFinal CaptureType = "NonFinal"
+	// Final ...
+	Final CaptureType = "Final"
+)
+
+func (c CaptureType) String() string {
+	return string(c)
+}
 
 // CapturesRequest ..
 type CapturesRequest struct {
-	Amount    uint64            `json:"amount,omitempty"`
-	Reference string            `json:"reference,omitempty"`
-	Metadata  map[string]string `json:"metadata,omitempty"`
+	Amount      uint64            `json:"amount,omitempty"`
+	Reference   string            `json:"reference,omitempty"`
+	CaptureType CaptureType       `json:"capture_type,omitempty"`
+	Metadata    map[string]string `json:"metadata,omitempty"`
 }
 
 // CapturesResponse ...

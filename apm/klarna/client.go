6949835daded2785eabd992f5137a769431b7d3f
Jamie Aitken <jamie.aitken93@gmail.com>
2023-08-01 18:37:21 +0100

    Add Context to Klarna client

--- apm/klarna/client.go
+++ apm/klarna/client.go
@@ -1,6 +1,7 @@
 package klarna
 
 import (
+	"context"
 	"github.com/checkout/checkout-sdk-go/client"
 	"github.com/checkout/checkout-sdk-go/common"
 	"github.com/checkout/checkout-sdk-go/configuration"
@@ -20,13 +21,18 @@ func NewClient(configuration *configuration.Configuration, apiClient client.Http
 }
 
 func (c *Client) CreateCreditSession(request CreditSessionRequest) (*CreditSessionResponse, error) {
+	return c.CreateCreditSessionWithContext(context.Background(), request)
+}
+
+func (c *Client) CreateCreditSessionWithContext(ctx context.Context, request CreditSessionRequest) (*CreditSessionResponse, error) {
 	auth, err := c.configuration.Credentials.GetAuthorization(configuration.PublicKey)
 	if err != nil {
 		return nil, err
 	}
 
 	var response CreditSessionResponse
-	err = c.apiClient.Post(
+	err = c.apiClient.PostWithContext(
+		ctx,
 		common.BuildPath(c.getBaseUrl(), creditSessionPath),
 		auth,
 		request,
@@ -41,13 +47,18 @@ func (c *Client) CreateCreditSession(request CreditSessionRequest) (*CreditSessi
 }
 
 func (c *Client) GetCreditSession(sessionId string) (*CreditSession, error) {
+	return c.GetCreditSessionWithContext(context.Background(), sessionId)
+}
+
+func (c *Client) GetCreditSessionWithContext(ctx context.Context, sessionId string) (*CreditSession, error) {
 	auth, err := c.configuration.Credentials.GetAuthorization(configuration.PublicKey)
 	if err != nil {
 		return nil, err
 	}
 
 	var response CreditSession
-	err = c.apiClient.Get(
+	err = c.apiClient.GetWithContext(
+		ctx,
 		common.BuildPath(c.getBaseUrl(), creditSessionPath, sessionId),
 		auth,
 		&response,
@@ -60,13 +71,18 @@ func (c *Client) GetCreditSession(sessionId string) (*CreditSession, error) {
 }
 
 func (c *Client) CapturePayment(paymentId string, request OrderCaptureRequest) (*CaptureResponse, error) {
+	return c.CapturePaymentWithContext(context.Background(), paymentId, request)
+}
+
+func (c *Client) CapturePaymentWithContext(ctx context.Context, paymentId string, request OrderCaptureRequest) (*CaptureResponse, error) {
 	auth, err := c.configuration.Credentials.GetAuthorization(configuration.SecretKey)
 	if err != nil {
 		return nil, err
 	}
 
 	var response CaptureResponse
-	err = c.apiClient.Post(
+	err = c.apiClient.PostWithContext(
+		ctx,
 		common.BuildPath(c.getBaseUrl(), ordersPath, paymentId, capturesPath),
 		auth,
 		request,
@@ -81,13 +97,18 @@ func (c *Client) CapturePayment(paymentId string, request OrderCaptureRequest) (
 }
 
 func (c *Client) VoidPayment(paymentId string, request payments.VoidRequest) (*payments.VoidResponse, error) {
+	return c.VoidPaymentWithContext(context.Background(), paymentId, request)
+}
+
+func (c *Client) VoidPaymentWithContext(ctx context.Context, paymentId string, request payments.VoidRequest) (*payments.VoidResponse, error) {
 	auth, err := c.configuration.Credentials.GetAuthorization(configuration.SecretKey)
 	if err != nil {
 		return nil, err
 	}
 
 	var response payments.VoidResponse
-	err = c.apiClient.Post(
+	err = c.apiClient.PostWithContext(
+		ctx,
 		common.BuildPath(c.getBaseUrl(), ordersPath, paymentId, voidsPath),
 		auth,
 		request,

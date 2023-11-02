d1b7a8572fe2f8b1084328078fb65ad4ba543381
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2023-11-02 14:15:12 +0100

    Update capture payment request param to optional

--- test/payments_captures_test.go
+++ test/payments_captures_test.go
@@ -67,6 +67,37 @@ func TestCaptureCardPayment(t *testing.T) {
 	}
 }
 
+func TestCaptureCardPaymentWithoutRequest(t *testing.T) {
+	paymentResponse := makeCardPayment(t, false, 10)
+
+	cases := []struct {
+		name       string
+		paymentId  string
+		checkerOne func(*payments.CaptureResponse, error)
+	}{
+		{
+			name:      "when get a capture payment request without request then return a response",
+			paymentId: paymentResponse.Id,
+			checkerOne: func(response *payments.CaptureResponse, err error) {
+				assert.Nil(t, err)
+				assert.NotNil(t, response)
+				assert.NotEmpty(t, response.ActionId)
+				assert.NotEmpty(t, response.Links)
+				assert.NotEmpty(t, response.Links["payment"])
+			},
+		},
+	}
+
+	client := DefaultApi().Payments
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			Wait(time.Duration(3))
+			tc.checkerOne(client.CapturePaymentWithoutRequest(tc.paymentId, nil))
+		})
+	}
+}
+
 func TestCaptureCardPaymentIdempotently(t *testing.T) {
 	paymentResponse := makeCardPayment(t, false, 10)
 
@@ -113,7 +144,9 @@ func TestCaptureCardPaymentIdempotently(t *testing.T) {
 			checker: func(response1 interface{}, err1 error, response2 interface{}, err2 error) {
 				assert.Nil(t, err1)
 				assert.NotNil(t, response1)
-				assert.NotNil(t, err2)
+				assert.Nil(t, err2)
+				assert.NotNil(t, response2)
+				assert.NotEqual(t, response1.(*payments.CaptureResponse).ActionId, response2.(*payments.CaptureResponse).ActionId)
 			},
 		},
 	}
@@ -132,6 +165,75 @@ func TestCaptureCardPaymentIdempotently(t *testing.T) {
 
 			processTwo := func() (interface{}, error) {
 				return client.CapturePayment(tc.paymentId, tc.captureRequest, &tc.idempotencyKeyRandom2)
+			}
+			predicateTwo := func(data interface{}) bool {
+				response := data.(*payments.CaptureResponse)
+				return response.Links != nil && len(response.Links) >= 0
+			}
+
+			retriableOne, errOne := retriable(processOne, predicateOne, 2)
+			retriableTwo, errTwo := retriable(processTwo, predicateTwo, 2)
+			tc.checker(retriableOne, errOne, retriableTwo, errTwo)
+		})
+	}
+}
+
+func TestCaptureCardPaymentWithoutrequestIdempotently(t *testing.T) {
+	paymentResponse := makeCardPayment(t, false, 10)
+
+	idempotencyKeyRandom1 := uuid.New().String()
+
+	idempotencyKeyRandom2 := uuid.New().String()
+
+	cases := []struct {
+		name                  string
+		paymentId             string
+		idempotencyKeyRandom1 string
+		idempotencyKeyRandom2 string
+		checker               func(interface{}, error, interface{}, error)
+	}{
+		{
+			name:                  "when request is valid then capture payment without request idempotently",
+			paymentId:             paymentResponse.Id,
+			idempotencyKeyRandom1: idempotencyKeyRandom1,
+			idempotencyKeyRandom2: idempotencyKeyRandom1,
+			checker: func(response1 interface{}, err1 error, response2 interface{}, err2 error) {
+				assert.Nil(t, err1)
+				assert.NotNil(t, response1)
+				assert.Nil(t, err2)
+				assert.NotNil(t, response2)
+				assert.Equal(t, response1.(*payments.CaptureResponse).ActionId, response2.(*payments.CaptureResponse).ActionId)
+			},
+		},
+		{
+			name:                  "when request is valid then capture without request payment idempotently error",
+			paymentId:             paymentResponse.Id,
+			idempotencyKeyRandom1: idempotencyKeyRandom1,
+			idempotencyKeyRandom2: idempotencyKeyRandom2,
+			checker: func(response1 interface{}, err1 error, response2 interface{}, err2 error) {
+				assert.Nil(t, err1)
+				assert.NotNil(t, response1)
+				assert.Nil(t, err2)
+				assert.NotNil(t, response2)
+				assert.NotEqual(t, response1.(*payments.CaptureResponse).ActionId, response2.(*payments.CaptureResponse).ActionId)
+			},
+		},
+	}
+
+	client := DefaultApi().Payments
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			processOne := func() (interface{}, error) {
+				return client.CapturePaymentWithoutRequest(tc.paymentId, &tc.idempotencyKeyRandom1)
+			}
+			predicateOne := func(data interface{}) bool {
+				response := data.(*payments.CaptureResponse)
+				return response.Links != nil && len(response.Links) >= 0
+			}
+
+			processTwo := func() (interface{}, error) {
+				return client.CapturePaymentWithoutRequest(tc.paymentId, &tc.idempotencyKeyRandom2)
 			}
 			predicateTwo := func(data interface{}) bool {
 				response := data.(*payments.CaptureResponse)

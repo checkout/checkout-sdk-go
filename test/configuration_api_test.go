74edbcc111bf6e52c1b344530cca2af209e64e0d
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-06-06 11:18:33 +0200

    Update length subdomain names validation

--- test/configuration_api_test.go
+++ test/configuration_api_test.go
@@ -10,45 +10,52 @@ import (
 	"github.com/checkout/checkout-sdk-go/mocks"
 )
 
-func TestShouldCreateConfiguration(t *testing.T) {
+func TestShouldCreateConfigurationWithSubdomain(t *testing.T) {
 	credentials := new(mocks.CredentialsMock)
-	environment := configuration.Production()
-	subdomain := configuration.NewEnvironmentSubdomain(environment, "123dmain")
-	subdomainBad := configuration.NewEnvironmentSubdomain(environment, "baddomain")
+	environment := configuration.Sandbox()
 
-	cases := []struct {
-		name          string
-		configuration *configuration.Configuration
-		checker       func(*configuration.Configuration, error)
+	testCases := []struct {
+		subdomain   string
+		expectedUrl string
 	}{
-		{
-			name:          "should create a configuration object",
-			configuration: configuration.NewConfiguration(credentials, environment, &http.Client{}, nil),
-			checker: func(configuration *configuration.Configuration, err error) {
-				assert.NotNil(t, configuration)
-			},
-		},
-		{
-			name:          "should create a configuration object with a valid subdomain",
-			configuration: configuration.NewConfigurationWithSubdomain(credentials, environment, subdomain, &http.Client{}, nil),
-			checker: func(configuration *configuration.Configuration, err error) {
-				assert.NotNil(t, configuration)
-				assert.Equal(t, "https://123dmain.api.checkout.com", configuration.EnvironmentSubdomain.ApiUrl)
-			},
-		},
-		{
-			name:          "should create a configuration object with a invalid subdomain",
-			configuration: configuration.NewConfigurationWithSubdomain(credentials, environment, subdomainBad, &http.Client{}, nil),
-			checker: func(configuration *configuration.Configuration, err error) {
-				assert.NotNil(t, configuration)
-				assert.Equal(t, "https://api.checkout.com", configuration.EnvironmentSubdomain.ApiUrl)
-			},
-		},
+		{"123dmain", "https://123dmain.api.sandbox.checkout.com"},
+		{"123domain", "https://123domain.api.sandbox.checkout.com"},
+		{"1234domain", "https://1234domain.api.sandbox.checkout.com"},
+		{"12345domain", "https://12345domain.api.sandbox.checkout.com"},
 	}
 
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.checker(tc.configuration, nil)
+	for _, tc := range testCases {
+		t.Run("Should create configuration with subdomain "+tc.subdomain, func(t *testing.T) {
+			subdomain := configuration.NewEnvironmentSubdomain(environment, tc.subdomain)
+			config := configuration.NewConfigurationWithSubdomain(credentials, environment, subdomain, &http.Client{}, nil)
+
+			assert.NotNil(t, config)
+			assert.Equal(t, tc.expectedUrl, config.EnvironmentSubdomain.ApiUrl)
+		})
+	}
+}
+
+func TestShouldCreateConfigurationWithBadSubdomain(t *testing.T) {
+	credentials := new(mocks.CredentialsMock)
+	environment := configuration.Sandbox()
+
+	testCases := []struct {
+		subdomain   string
+		expectedUrl string
+	}{
+		{"", "https://api.sandbox.checkout.com"},
+		{"123", "https://api.sandbox.checkout.com"},
+		{"123bad", "https://api.sandbox.checkout.com"},
+		{"12345domainBad", "https://api.sandbox.checkout.com"},
+	}
+
+	for _, tc := range testCases {
+		t.Run("Should create configuration with bad subdomain "+tc.subdomain, func(t *testing.T) {
+			subdomain := configuration.NewEnvironmentSubdomain(environment, tc.subdomain)
+			config := configuration.NewConfigurationWithSubdomain(credentials, environment, subdomain, &http.Client{}, nil)
+
+			assert.NotNil(t, config)
+			assert.Equal(t, tc.expectedUrl, config.EnvironmentSubdomain.ApiUrl)
 		})
 	}
 }

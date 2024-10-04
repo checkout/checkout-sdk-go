4e9d919a9e8b867e17e33442681f42128b9d771c
Jamie Aitken <jamie.aitken93@gmail.com>
2024-10-04 08:44:42 +0100

    Add context for all API interactions (#86)
    * Add Context to HTTP layer
    
    * Add Context to Customers client
    
    * Add Context to Disputes client
    
    * Add Context to Events client
    
    * Add Context to Hosted client
    
    * Add Context to Instruments client
    
    * Add Context to Links client
    
    * Add Context to Payments client
    
    * Add Context to Reconciliation client
    
    * Add Context to Sources client
    
    * Add Context to Tokens client
    
    * Add Context to Webhook client
    
    * Add Context to iDEAL client
    
    * Add Context to Klarna client
    
    * Add Context to SEPA client
    
    * Update tests
    
    * Add Context to NAS Payments
    
    ---------
    
    Co-authored-by: Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>

--- instruments/abc/client.go
+++ instruments/abc/client.go
@@ -1,6 +1,7 @@
 package abc
 
 import (
+	"context"
 	"github.com/checkout/checkout-sdk-go/client"
 	"github.com/checkout/checkout-sdk-go/common"
 	"github.com/checkout/checkout-sdk-go/configuration"
@@ -20,13 +21,18 @@ func NewClient(configuration *configuration.Configuration, apiClient client.Http
 }
 
 func (c *Client) Create(request CreateInstrumentRequest) (*CreateInstrumentResponse, error) {
+	return c.CreateWithContext(context.Background(), request)
+}
+
+func (c *Client) CreateWithContext(ctx context.Context, request CreateInstrumentRequest) (*CreateInstrumentResponse, error) {
 	auth, err := c.configuration.Credentials.GetAuthorization(configuration.SecretKey)
 	if err != nil {
 		return nil, err
 	}
 
 	var response CreateInstrumentResponse
-	err = c.apiClient.Post(
+	err = c.apiClient.PostWithContext(
+		ctx,
 		common.BuildPath(instruments.Path),
 		auth,
 		request,
@@ -41,13 +47,17 @@ func (c *Client) Create(request CreateInstrumentRequest) (*CreateInstrumentRespo
 }
 
 func (c *Client) Get(instrumentId string) (*GetInstrumentResponse, error) {
+	return c.GetWithContext(context.Background(), instrumentId)
+}
+
+func (c *Client) GetWithContext(ctx context.Context, instrumentId string) (*GetInstrumentResponse, error) {
 	auth, err := c.configuration.Credentials.GetAuthorization(configuration.SecretKey)
 	if err != nil {
 		return nil, err
 	}
 
 	var response GetInstrumentResponse
-	err = c.apiClient.Get(common.BuildPath(instruments.Path, instrumentId), auth, &response)
+	err = c.apiClient.GetWithContext(ctx, common.BuildPath(instruments.Path, instrumentId), auth, &response)
 	if err != nil {
 		return nil, err
 	}
@@ -56,13 +66,18 @@ func (c *Client) Get(instrumentId string) (*GetInstrumentResponse, error) {
 }
 
 func (c *Client) Update(instrumentId string, request UpdateInstrumentRequest) (*UpdateInstrumentResponse, error) {
+	return c.UpdateWithContext(context.Background(), instrumentId, request)
+}
+
+func (c *Client) UpdateWithContext(ctx context.Context, instrumentId string, request UpdateInstrumentRequest) (*UpdateInstrumentResponse, error) {
 	auth, err := c.configuration.Credentials.GetAuthorization(configuration.SecretKey)
 	if err != nil {
 		return nil, err
 	}
 
 	var response UpdateInstrumentResponse
-	err = c.apiClient.Patch(
+	err = c.apiClient.PatchWithContext(
+		ctx,
 		common.BuildPath(instruments.Path, instrumentId),
 		auth,
 		request,
@@ -76,13 +91,17 @@ func (c *Client) Update(instrumentId string, request UpdateInstrumentRequest) (*
 }
 
 func (c *Client) Delete(instrumentId string) (*common.MetadataResponse, error) {
+	return c.DeleteWithContext(context.Background(), instrumentId)
+}
+
+func (c *Client) DeleteWithContext(ctx context.Context, instrumentId string) (*common.MetadataResponse, error) {
 	auth, err := c.configuration.Credentials.GetAuthorization(configuration.SecretKey)
 	if err != nil {
 		return nil, err
 	}
 
 	var response common.MetadataResponse
-	err = c.apiClient.Delete(common.BuildPath(instruments.Path, instrumentId), auth, &response)
+	err = c.apiClient.DeleteWithContext(ctx, common.BuildPath(instruments.Path, instrumentId), auth, &response)
 	if err != nil {
 		return nil, err
 	}
